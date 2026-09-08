e8e803fd993ed214ea0c0d1a9e1dce392539b799
carlossescalante79-ctrl <carlossescalante79@gmail.com>
2026-09-07 20:22:30 -0600

    Update qr.js

--- api/qr.js
+++ api/qr.js
@@ -1,10 +1,16 @@
 import QRCode from 'qrcode';
+
 export default async function handler(req,res){
-  const proto=(req.headers['x-forwarded-proto']||'https').split(',')[0];
-  const host=req.headers.host;
-  const url=`${proto}://${host}/registro`;
-  const svg=await QRCode.toString(url,{type:'svg',margin:1,width:800,errorCorrectionLevel:'M'});
+  const url='https://bhg-control-personal.vercel.app/registro';
+
+  const svg=await QRCode.toString(url,{
+    type:'svg',
+    margin:1,
+    width:800,
+    errorCorrectionLevel:'M'
+  });
+
   res.setHeader('Content-Type','image/svg+xml');
-  res.setHeader('Cache-Control','public, max-age=3600');
+  res.setHeader('Cache-Control','no-store');
   res.status(200).send(svg);
 }
